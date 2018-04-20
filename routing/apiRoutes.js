9bda1f3df0152130ada79642c052717df9374a27
Falon Darville <falondarville@gmail.com>
2018-04-20 10:06:38 -0700

    start html portion

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -13,6 +13,12 @@ router.get('/friends', function(request, response){
 // this route handles compatibility logic
 router.post('/friends', function(request, response){
 	var newResults = request.body.friends;
+	// var a = friends array from friends.js
+	// var b = array compiled through the quiz taken by the user
+	// calculate compatibility here to the friends.js file
+	// var difference = function (a, b) { 
+	// 	return Math.abs(a - b);
+	// };
 	console.log(newResults);
 });
 
