7cfd1df5384410f8e614876688b07efbfb1f9b34
Falon Darville <falondarville@gmail.com>
2018-04-21 20:08:57 -0700

    add seperate js file

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -12,14 +12,22 @@ router.get('/friends', function(request, response){
 // post route that handles incoming survey results
 // this route handles compatibility logic
 router.post('/friends', function(request, response){
-	var newResults = request.body.friends;
+	var name = request.body.name;
+	var scores = request.body.scores;
+	var bestMatch = {};
+
+	for(var i = 0; i < friends.length; i++){
+		console.log(friends[i]);
+
+	};
+
+	return response.json(bestMatch);
 	// var a = friends array from friends.js
 	// var b = array compiled through the quiz taken by the user
 	// calculate compatibility here to the friends.js file
 	// var difference = function (a, b) { 
 	// 	return Math.abs(a - b);
 	// };
-	console.log(newResults);
 });
 
 module.exports = router;
